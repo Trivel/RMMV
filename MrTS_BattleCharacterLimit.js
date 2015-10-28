7887a231b9a8d6281a1c2d986e42e3a69de8d349
Mr Trivel <TrivelWindrynn@gmail.com>
2015-10-28 10:51:59 +0200

    Update MrTS_BattleCharacterLimit.js

--- MrTS_BattleCharacterLimit.js
+++ MrTS_BattleCharacterLimit.js
@@ -3,7 +3,7 @@
 //==============================================================================
 
 /*:
-* @plugindesc Adds color coding to items.
+* @plugindesc Changes character placement for battles.
 * @author Mr. Trivel
 * 
 * @param Max Characters
@@ -14,20 +14,31 @@
 * @desc Characters per row in battle
 * @default 3
 *
+* @param Offset
+* @desc Starting horizontal offset of characters
+* @default 120
+* 
+* @param Vertical Offset
+* @desc Starting vertical offset of characters
+* @default 360
+* 
 * @param Lower Index
 * @desc How further each character below is
 * @default 48
 *
 * @param Forward Offset
-* @desc How forward should characters move per row
-* @default 50
+* @desc Move the rows by an offset for each row in battle
+* @default 100
 *
 * @param Row Spacing
 * @desc Space between rows
 * @default 100
+*
+* @param Vertical Chara Spacing
+* @desc Space between rows
+* @default 54
 * 
-* @help This plugin does not provide plugin commands.
-* Version 1.0
+* @help Version 1.1
 */
 
 (function() {
@@ -35,9 +46,12 @@
 
 	var maxCharacters = Number(parameters['Max Characters'] || 5);
 	var charasPerRow = Number(parameters['Characters Per Row'] || 3);
+	var rightOffset = Number(parameters['Offset'] || 360);
+	var topOffset = Number(parameters['Vertical Offset'] || 120);
 	var lowerCharaIndex = Number(parameters['Lower Index'] || 48);
-	var forwardIndex = Number(parameters['Forward Offset'] || 50);
+	var forwardIndex = Number(parameters['Forward Offset'] || 100);
 	var rowSpacing = Number(parameters['Row Spacing'] || 100);
+	var charaVerticalSpacing = Number(parameters['Vertical Chara Spacing'] || 54);
 
 	Game_Party.prototype.maxBattleMembers = function() {
 	    return maxCharacters;
@@ -45,8 +59,8 @@
 
 	Sprite_Actor.prototype.setActorHome = function(index) {
 		var c = Math.floor(index/charasPerRow);
-		var x = (600 - forwardIndex * Math.floor($gameParty.battleMembers().length/charasPerRow)) + lowerCharaIndex * (index % 3) + rowSpacing * c;
-		var y = 280 + (index%charasPerRow) * 48;
+		var x = (Graphics.boxWidth - rightOffset) - forwardIndex * Math.floor($gameParty.battleMembers().length/charasPerRow) + lowerCharaIndex * (index % charasPerRow) + rowSpacing * c;
+		var y = topOffset + (index%charasPerRow) * charaVerticalSpacing;
 	    this.setHome(x, y);
 	};
 
