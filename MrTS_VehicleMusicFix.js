14b46564d5f4a9a1ab4cd45e6608af3768e79957
Mr Trivel <TrivelWindrynn@gmail.com>
2016-01-27 14:53:58 +0200

    Create MrTS_VehicleMusicFix.js

---
+++ MrTS_VehicleMusicFix.js
@@ -0,0 +1,49 @@
+//=============================================================================
+// MrTS_VehicleMusicFix.js
+//=============================================================================
+
+/*:
+* @plugindesc Changes how music and vehicles work.
+* @author Mr. Trivel
+* 
+* @help 
+* --------------------------------------------------------------------------------
+* Free for commercial and commercial use.
+* Version 1.0
+* --------------------------------------------------------------------------------
+*
+* --------------------------------------------------------------------------------
+* After boarding onto a vehicle and going to a new map, it'd play the autoplay
+* BGM there. Now it keeps vehicle BGM playing.
+*
+* After boaring onto a vehicle in one map and leaving it in another, it'd play the
+* BGM from previous map. Now it plays the BGM of the map which you left vehicle.
+* --------------------------------------------------------------------------------
+* 
+* --------------------------------------------------------------------------------
+* Version History
+* --------------------------------------------------------------------------------
+* 1.0 - Release
+*/
+
+(function() {
+
+	var _GameMap_autoplay = Game_Map.prototype.autoplay;
+	Game_Map.prototype.autoplay = function() {
+		if (!$gamePlayer.isInVehicle())
+			_GameMap_autoplay.call(this);
+	};
+
+	var _GameVehicle_getOn = Game_Vehicle.prototype.getOn;
+	Game_Vehicle.prototype.getOn = function() {
+	    _GameVehicle_getOn.call(this);
+	    this._boardedOnMapId = $gameMap.mapId();
+	};
+
+	var _GameVehicle_getOff = Game_Vehicle.prototype.getOff;
+	Game_Vehicle.prototype.getOff = function() {
+		_GameVehicle_getOff.call(this);
+		if (this._boardedOnMapId !== $gameMap.mapId())
+			_GameMap_autoplay.call($gameMap);
+	};
+})();
