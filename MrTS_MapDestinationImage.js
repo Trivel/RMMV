e5cf7ab5cf6caa8e4ae812fd123741af2aab88ed
Mr Trivel <TrivelWindrynn@gmail.com>
2016-02-27 22:40:09 +0200

    Update and rename MrTS_SimpleMapDestination.js to MrTS_MapDestinationImage.js

--- MrTS_MapDestinationImage.js
+++ MrTS_MapDestinationImage.js
@@ -1,15 +1,30 @@
 //=============================================================================
-// MrTS_SimpleMapDestination.js
+// MrTS_MapDestinationImage.js
 //=============================================================================
 
 /*:
-* @plugindesc Changes map destination graphic and can disable it's effect.
+* @plugindesc Changes map destination graphic and can disable it's effect. Can be animated.
 * @author Mr. Trivel
 *
 * @param Disable Effect
-* @desc Should effect be disabled? (true/false)
+* @desc Should default effect be disabled? (true/false)
 * Default: True
 * @default True
+*
+* @param Animated Destination
+* @desc Do you want destination image animated? (true/false)
+* Default: False
+* @default False
+*
+* @param Frames
+* @desc If animated. How many frames in the image?
+* Default: 4
+* @default 4
+*
+* @param Speed
+* @desc If animated. How fast is the animation? Every.. frames
+* Default: 10
+* @default 10
 * 
 * @help 
 * --------------------------------------------------------------------------------
@@ -19,7 +34,7 @@
 * Credit Mr. Trivel if using this plugin in your project.
 * Free for commercial and non-commercial projects.
 * --------------------------------------------------------------------------------
-* Version 1.0
+* Version 1.1
 * --------------------------------------------------------------------------------
 *
 * --------------------------------------------------------------------------------
@@ -33,12 +48,24 @@
 * --------------------------------------------------------------------------------
 * Version History
 * --------------------------------------------------------------------------------
+* 1.1 - Destination image can be animated.
 * 1.0 - Release
 */
 
 (function() {
-	var parameters = PluginManager.parameters('MrTS_SimpleMapDestination');
+	var parameters = PluginManager.parameters('MrTS_MapDestinationImage');
 	var paramDisableEff = (parameters['Disable Effect'] || "true").toLowerCase() === "true";
+	var paramAnimated = (parameters['Animated Destination'] || "False").toLowerCase() === "true";
+	var paramAnimFrames = Number(parameters['Frames'] || 4);
+	var paramAnimSpeed = Number(parameters['Speed'] || 10);
+
+	var _SpriteDestination_initialize = Sprite_Destination.prototype.initialize;
+	Sprite_Destination.prototype.initialize = function() {
+		_SpriteDestination_initialize.call(this);
+		this._frameNumber = 0;
+		this._frameWidth = 48;
+		this._loadedImage = false;
+	};
 
 	Sprite_Destination.prototype.createBitmap = function() {
 	    this.bitmap = ImageManager.loadSystem('mapDestination');
@@ -48,7 +75,25 @@
 
 	var _SpriteDestination_updateAnimation = Sprite_Destination.prototype.updateAnimation;
 	Sprite_Destination.prototype.updateAnimation = function() {
-		if (paramDisableEff) return;
-		_SpriteDestination_updateAnimation.call(this);
+		if (!this._loadedImage && paramAnimated && this.bitmap.isReady())
+		{
+			this._frameWidth = this.bitmap.width/paramAnimFrames;
+			this.setFrame(this._frameWidth*this._frameNumber, 0, this._frameWidth, this.bitmap.height);
+			this._loadedImage = true;
+		}
+
+		if (paramAnimated && this._loadedImage)
+		{
+			this._frameCount++;
+			if (this._frameCount >= paramAnimSpeed)
+			{
+				this._frameCount = 0;
+				this._frameNumber++;
+				if (this._frameNumber >= paramAnimFrames) this._frameNumber = 0;
+				this.setFrame(this._frameWidth*this._frameNumber, 0, this._frameWidth, this.bitmap.height);
+			}
+		}
+
+		if (!paramDisableEff) _SpriteDestination_updateAnimation.call(this);
 	};
 })();
