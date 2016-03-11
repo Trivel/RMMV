7cd5a63c4fdbe935e55d9b2a0a8cd04735976908
Mr Trivel <TrivelWindrynn@gmail.com>
2016-03-11 15:52:56 +0200

    Update MrTS_RareEnemies.js

--- MrTS_RareEnemies.js
+++ MrTS_RareEnemies.js
@@ -14,24 +14,35 @@
 * Credit Mr. Trivel if using this plugin in your project.
 * Free for commercial and non-commercial projects.
 * --------------------------------------------------------------------------------
-* Version 1.0
+* Version 1.1
 * --------------------------------------------------------------------------------
 *
 * It requires two commands used in enemies note fields:
 * <re_id: X> - ID of rare enemy
 * <re_chc: Y.YY> - Chance of appearing - 0.01 - 1%, 1.00 - 100%, 0.5 - 50%
+* --------------------------------------------------------------------------------
+*
+* --------------------------------------------------------------------------------
+* Version History
+* --------------------------------------------------------------------------------
+* 1.1 - Fixed rare enemy retaining old id's HP.
+* 1.0 - Release
 */
 
 (function() {
 	var _Game_Enemy_setup = Game_Enemy.prototype.setup;
 
 	Game_Enemy.prototype.setup = function(enemyId, x, y) {
-		_Game_Enemy_setup.call(this, enemyId, x, y);
-		var chance = Number(this.enemy().meta.re_chc);
-		if (Math.random() < chance)
+		if ($dataEnemies[enemyId].meta.re_chc)
 		{
-			this._enemyId = Number(this.enemy().meta.re_id);
+			var chance = Number($dataEnemies[enemyId].meta.re_chc);
+			if (Math.random() < chance)
+			{
+				enemyId = Number($dataEnemies[enemyId].meta.re_id);
+			}
 		}
+		_Game_Enemy_setup.call(this, enemyId, x, y);
+		
 
 	};
 })();
