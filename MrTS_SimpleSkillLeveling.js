4d8821d5aad0dc80bd59481632f16d7b4e39ec6b
Mr Trivel <TrivelWindrynn@gmail.com>
2016-04-06 17:06:48 +0300

    Update MrTS_SimpleSkillLeveling.js

--- MrTS_SimpleSkillLeveling.js
+++ MrTS_SimpleSkillLeveling.js
@@ -15,20 +15,44 @@
 * Free for non-commercial projects.
 * For commercial use contact Mr. Trivel.
 * --------------------------------------------------------------------------------
-* Version 1.0
+* Version 1.1
 * --------------------------------------------------------------------------------
 ** 
 * --------------------------------------------------------------------------------
 * Skill Tags
 * --------------------------------------------------------------------------------
 * To make skill change after some uses, use the following tag:
-* <LevelUpTo: [ID], [USES]>
+* <LevelUpTo: [ID], [USES], [REMOVE]>
+*
+* Will make skill require a specific other existing skill that was used.
+* <LevelUpRequire: [ID], [USES], [REMOVE]>
+* 
 * [ID] - Skill ID to change to
 * [USES] - After how many uses change into that skill
+* [REMOVE] - 0 - keep skill after leveling up, 1 - remove skill after leveling up
+* (Remove is optional, by default it'll remove)
+* 
+* Examples:
+* <LevelUpTo: 5, 25, 0>
+* <LeveLUpTo: 8, 10>
+*
+* <LevelUpTo: 5, 20, 0>
+* <LevelUpRequire: 7, 5>
+* <LevelUpRequire: 8, 9>
+*
+* <LevelUpTo: 7, 20>
+* <LevelUpRequire: 8, 10, 0>
+*
+* <LevelUpTo: 8, 10>
+* <LevelUpRequire: 7, 20> 
+* --------------------------------------------------------------------------------
 * 
 * --------------------------------------------------------------------------------
 * Version History
 * --------------------------------------------------------------------------------
+* 1.1 - Added multiple skill requirement for level up.
+*     - Added ability to keep skills after they leveled up.
+*     - Fixed a bug where it'd count as multiple uses for multiple target skill.
 * 1.0 - Release
 */
 
@@ -37,41 +61,85 @@
 	Game_Actor.prototype.initMembers = function() {
 		_GameActor_initMembers.call(this);
 	    Game_Battler.prototype.initMembers.call(this);
-	    this._leveledSkills = {};
+	    this._skillUsedTimes = {};
+	    this._levelingSkillNeeds = [];
 	};
 
 	var _GameActor_learnSkill = Game_Actor.prototype.learnSkill;
 	Game_Actor.prototype.learnSkill = function(skillId) {
 		if (!this.isLearnedSkill(skillId) && $dataSkills[skillId].meta.LevelUpTo)
 		{
-			this._leveledSkills[skillId] = 0;
+			this.grabSkillLevelingData(skillId);
+			this._skillUsedTimes[skillId] = 0;
 		}
 		_GameActor_learnSkill.call(this, skillId);
 	};
 
-	Game_Actor.prototype.increaseSkillUsage = function(skill) {
-		if (!skill.meta.LevelUpTo)
-			return;
+	Game_Actor.prototype.grabSkillLevelingData = function(skillId) {
+		var lines = $dataSkills[skillId].note.split(/[\r\n]/);
+		var skillLearnObj = {result: 0, required: [], completed: false};
+		var regex1 = /<LevelUpTo:[ ]*(\d+),[ ]*(\d+),?[ ]?(\d+)?>/i;
+		var regex2 = /<LevelUpRequire:[ ]*(\d+),[ ]*(\d+),?[ ]?(\d+)?>/i;
+		for (var i = 0; i < lines.length; i++) {
+			var regex1Match = regex1.exec(lines[i]);
+			if (regex1Match)
+			{
+				var goal = Number(regex1Match[1]);
+				var reqId = skillId;
+				var uses = Number(regex1Match[2]);
+				var remove = regex1Match[3] ? regex1Match[3] === "1" : true;
+				skillLearnObj.result = goal;
+				skillLearnObj.required.push({id: reqId, uses: uses, remove: remove});
+				continue;
+			}
 
-		var data = skill.meta.LevelUpTo.split(",");
-		var lto  = Number(data[0]);
-		var uses = Number(data[1]);
+			var regex2Match = regex2.exec(lines[i]);
+			if (regex2Match)
+			{
+				var reqId = Number(regex2Match[1]);
+				var uses = Number(regex2Match[2]);
+				var remove = regex2Match[3] ? regex2Match[3] === "1" : true;
+				skillLearnObj.required.push({id: reqId, uses: uses, remove: remove});
+				continue;
+			}
+		}
+		this._levelingSkillNeeds.push(skillLearnObj);
+	};
 
-		this._leveledSkills[skill.id]++;
-		if (this._leveledSkills[skill.id] >= uses)
-		{
-			this._leveledSkills[skill.id] = 0;
-			this.forgetSkill(skill.id);
-			this.learnSkill(lto);
+	Game_Actor.prototype.checkSkillLevelUps = function() {
+		for (var i = 0; i < this._levelingSkillNeeds.length; i++) {
+			var skill = this._levelingSkillNeeds[i];
+			if (skill.completed) continue;
+			var allDone = true;
+			for (var j = 0; j < skill.required.length; j++) {
+				var req = skill.required[j];
+				if (!this._skillUsedTimes[req.id]) allDone = false;
+				if (this._skillUsedTimes[req.id] < req.uses) allDone = false;
+				if (!allDone) break;
+			}
+			if (allDone)
+			{
+				this.learnSkill(skill.result);
+				this._levelingSkillNeeds[i].completed = true;
+				for (var j = 0; j < skill.required.length; j++) {
+					if(skill.required[j].remove) this.forgetSkill(skill.required[j].id);
+				}
+			}
 		}
 	};
 
-	var _GameAction_apply = Game_Action.prototype.apply;
-	Game_Action.prototype.apply = function(target) {
-		_GameAction_apply.call(this, target);
-		if (this.subject().isActor() && DataManager.isSkill(this.item()))
+	Game_Actor.prototype.increaseSkillUsage = function(skill) {
+		if (!this._skillUsedTimes[skill.id]) this._skillUsedTimes[skill.id] = 0;
+		this._skillUsedTimes[skill.id]++;
+		this.checkSkillLevelUps();
+	};
+
+	var _Game_Actor_useItem = Game_Actor.prototype.useItem;
+	Game_Actor.prototype.useItem = function(item) {
+		_Game_Actor_useItem.call(this, item);
+		if (DataManager.isSkill(item))
 		{
-			this.subject().increaseSkillUsage(this.item());
+			this.increaseSkillUsage(item);
 		}
 	};
 })();
