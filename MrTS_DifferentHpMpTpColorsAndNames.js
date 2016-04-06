4fe2e815e4b610568d3a6a586ade12c25596416f
Mr Trivel <TrivelWindrynn@gmail.com>
2016-04-06 20:58:41 +0300

    Update MrTS_DifferentHpMpTpColorsAndNames.js

--- MrTS_DifferentHpMpTpColorsAndNames.js
+++ MrTS_DifferentHpMpTpColorsAndNames.js
@@ -2,6 +2,9 @@
 // MrTS_DifferentHpMpTpColorsAndNames.js
 //=============================================================================
 
+var Imported = Imported || {};
+Imported.MrTS_DifferentHpMpTpColorsAndNames = true;
+
 /*:
 * @plugindesc Changes colors and names of HP, MP and TP. Purely graphical.
 * @author Mr. Trivel
@@ -14,133 +17,132 @@
 * Credit Mr. Trivel if using this plugin in your project.
 * Free for commercial and non-commercial projects.
 * --------------------------------------------------------------------------------
-* Version 1.0
+* Version 1.1
 * --------------------------------------------------------------------------------
 *
 * --------------------------------------------------------------------------------
-* How to set up
+* Actor/Class Tags
 * --------------------------------------------------------------------------------
-* Open the plugin in your favorite text editor and scroll down to
-* SET UP COLORS HERE
-* Follow the template there and change it to your needs.
+* To set colors for actors/classes, use the following tags in the note fields:
+* (Color is in HEX) (If Class and Actor has same tags, Class takes priority)
+* <HPName: Name>
+* <MPName: Name>
+* <TPName: Name>
+* <HPColor: #Color1, #Color2>
+* <MPColor: #Color1, #Color2>
+* <TPColor: #Color1, #Color2>
+*
+* Examples:
+* <HPName: QP>
+* <MPName: BP>
+* <TPName: RP>
+* <HPColor: #23b94d, #60e021>
+* <MPColor: #D02E2E, #ED5757>
+* <TPColor: #e29a00, #f8f4b2>
 * --------------------------------------------------------------------------------
 *
 * --------------------------------------------------------------------------------
 * Version History
 * --------------------------------------------------------------------------------
+* 1.1 - Moved color setting up to Database.
+*     - Added gauge colors to Classes.
 * 1.0 - Release
 */
 
 (function() {
-	//--------------------------------------------------------------------------------
-	// SET UP COLORS HERE v
-	//--------------------------------------------------------------------------------
-	// HP Color List
-	//--------------------------------------------------------------------------------
-	var hpColorList = {
-		// ActorId: ["HP Name", #hexcolor1, #hexcolor2],
-		// ActorId: ["HP Name", #hexcolor1, #hexcolor2]
-		1: ["QP", "#23b94d", "#60e021"],
-		2: ["QP", "#23b94d", "#60e021"],
-		3: ["QP", "#23b94d", "#60e021"],
-		4: ["QP", "#23b94d", "#60e021"]
-	}
-	//--------------------------------------------------------------------------------
-	// MP Color List
-	//--------------------------------------------------------------------------------
-	var mpColorList = {
-		// ActorId: ["MP Name", #hexcolor1, #hexcolor2],
-		// ActorId: ["MP Name", #hexcolor1, #hexcolor2]
-		1: ["MP", "#4080C0", "#40C0F0"],
-		2: ["SP", "#F718FC", "#FC69FF"],
-		3: ["RP", "#42FEC1", "#40FFF0"],
-		4: ["BP", "#D02E2E", "#ED5757"]
-	}
-	//--------------------------------------------------------------------------------
-	// TP Color List
-	//--------------------------------------------------------------------------------
-	var tpColorList = {
-		// ActorId: ["TP Name", #hexcolor1, #hexcolor2],
-		// ActorId: ["TP Name", #hexcolor1, #hexcolor2]
-		1: ["RP", "#e29a00", "#f8f4b2"],
-		2: ["RP", "#e29a00", "#f8f4b2"]
-	}
-	//--------------------------------------------------------------------------------
-	// SET UP COLORS THERE ^
-	//-------------------------------------------------------------------------------- 
-
 	Window_Base.prototype.setHpGaugeColors = function(actorId) {
-		if (hpColorList[actorId])
-		{
-			this._hpGaugeColor1 = hpColorList[actorId][1];
-			this._hpGaugeColor2 = hpColorList[actorId][2];
-		}
+		var actor = $gameActors.actor(actorId);
+		var aData = actor.actor();
+		var cData = actor.currentClass();
+		var colors = null;
+
+		if (cData.meta.HPColor)
+			colors = cData.meta.HPColor.split(',');
+		else if (aData.meta.HPColor)
+			colors = aData.meta.HPColor.split(',');
 		else
-		{
-			this._hpGaugeColor1 = this.textColor(20);
-			this._hpGaugeColor2 = this.textColor(21);
+			colors = [this.textColor(20), this.textColor(21)];
+
+		for (var i = 0; i < colors.length; i++) {
+			if (colors[i][0] === ' ') colors[i] = colors[i].slice(1);
 		}
+		
+		this._hpGaugeColors = colors;
 	};
 
 	Window_Base.prototype.setMpGaugeColors = function(actorId) {
-		if (mpColorList[actorId])
-		{
-			this._mpGaugeColor1 = mpColorList[actorId][1];
-			this._mpGaugeColor2 = mpColorList[actorId][2];
-		}
+		var actor = $gameActors.actor(actorId);
+		var aData = actor.actor();
+		var cData = actor.currentClass();
+		var colors = null;
+
+		if (cData.meta.MPColor)
+			colors = cData.meta.MPColor.split(',');
+		else if (aData.meta.MPColor)
+			colors = aData.meta.MPColor.split(',');
 		else
-		{
-			this._mpGaugeColor1 = this.textColor(22);
-			this._mpGaugeColor2 = this.textColor(23);
+			colors = [this.textColor(22), this.textColor(23)];
+
+		for (var i = 0; i < colors.length; i++) {
+			if (colors[i][0] === ' ') colors[i] = colors[i].slice(1);
 		}
+		
+		this._mpGaugeColors = colors;
 	};
 
 	Window_Base.prototype.setTpGaugeColors = function(actorId) {
-		if (tpColorList[actorId])
-		{
-			this._tpGaugeColor1 = tpColorList[actorId][1];
-			this._tpGaugeColor2 = tpColorList[actorId][2];
-		}
+		var actor = $gameActors.actor(actorId);
+		var aData = actor.actor();
+		var cData = actor.currentClass();
+		var colors = null;
+
+		if (cData.meta.TPColor)
+			colors = cData.meta.TPColor.split(',');
+		else if (aData.meta.TPColor)
+			colors = aData.meta.TPColor.split(',');
 		else
-		{
-			this._tpGaugeColor1 = this.textColor(28);
-			this._tpGaugeColor2 = this.textColor(29);
+			colors = [this.textColor(28), this.textColor(29)];
+
+		for (var i = 0; i < colors.length; i++) {
+			if (colors[i][0] === ' ') colors[i] = colors[i].slice(1);
 		}
+		
+		this._tpGaugeColors = colors;
 	};
 
 	var _WindowBase_hpGaugeColor1 = Window_Base.prototype.hpGaugeColor1;
 	Window_Base.prototype.hpGaugeColor1 = function() {
-		if (this._hpGaugeColor1) return this._hpGaugeColor1;
+		if (this._hpGaugeColors) return this._hpGaugeColors[0];
 		return _WindowBase_hpGaugeColor1.call(this);
 	};
 
 	var _WindowBase_hpGaugeColor2 = Window_Base.prototype.hpGaugeColor2;
 	Window_Base.prototype.hpGaugeColor2 = function() {
-		if (this._hpGaugeColor2) return this._hpGaugeColor2;
+		if (this._hpGaugeColors) return this._hpGaugeColors[1];
 		return _WindowBase_hpGaugeColor2.call(this);
 	};
 
 	var _WindowBase_mpGaugeColor1 = Window_Base.prototype.mpGaugeColor1;
 	Window_Base.prototype.mpGaugeColor1 = function() {
-		if (this._mpGaugeColor1) return this._mpGaugeColor1;
+		if (this._mpGaugeColors) return this._mpGaugeColors[0];
 		return _WindowBase_mpGaugeColor1.call(this);
 	};
 
 	var _WindowBase_mpGaugeColor2 = Window_Base.prototype.mpGaugeColor2;
 	Window_Base.prototype.mpGaugeColor2 = function() {
-		if (this._mpGaugeColor2) return this._mpGaugeColor2;
+		if (this._mpGaugeColors) return this._mpGaugeColors[1];
 		return _WindowBase_mpGaugeColor2.call(this);
 	};
 
 	var _WindowBase_tpGaugeColor1 = Window_Base.prototype.tpGaugeColor1;
 	Window_Base.prototype.tpGaugeColor1 = function() {
-		if (this._tpGaugeColor1) return this._tpGaugeColor1;
+		if (this._tpGaugeColors) return this._tpGaugeColors[0];
 		return _WindowBase_tpGaugeColor1.call(this);
 	};
 
 	var _WindowBase_tpGaugeColor2 = Window_Base.prototype.tpGaugeColor2;
 	Window_Base.prototype.tpGaugeColor2 = function() {
-		if (this._tpGaugeColor2) return this._tpGaugeColor2;
+		if (this._tpGaugeColors) return this._tpGaugeColors[1];
 		return _WindowBase_tpGaugeColor2.call(this);
 	};
 
@@ -213,28 +215,45 @@
 
 	Game_System.prototype.getActorTpName = function() {
 		if (this._actorTpName) return this._actorTpName;
-		console.log(TextManager.getter('basic', 7));
 		return TextManager.ctpA;
 	};
 
 	Game_System.prototype.setActorHpName = function(actorId) {
-		if (hpColorList[actorId])
-			this._actorHpName = hpColorList[actorId][0];
-		else
-			this._actorHpName = undefined;
+		var actor = $gameActors.actor(actorId);
+		var name = undefined;
+
+		if (actor.currentClass().meta.HPName)
+			name = actor.currentClass().meta.HPName;
+		if (actor.actor().meta.HPName)
+			name = actor.actor().meta.HPName;
+
+		if (name && name[0] === ' ') name = name.slice(1);
+		this._actorHpName = name;
 	};
 
 	Game_System.prototype.setActorMpName = function(actorId) {
-		if (mpColorList[actorId])
-			this._actorMpName = mpColorList[actorId][0];
-		else
-			this._actorMpName = undefined;
+		var actor = $gameActors.actor(actorId);
+		var name = undefined;
+
+		if (actor.currentClass().meta.MPName)
+			name = actor.currentClass().meta.MPName;
+		if (actor.actor().meta.MPName)
+			name = actor.actor().meta.MPName;
+
+		if (name && name[0] === ' ') name = name.slice(1);
+		this._actorMpName = name;
 	};
 
 	Game_System.prototype.setActorTpName = function(actorId) {
-		if (tpColorList[actorId])
-			this._actorTpName = tpColorList[actorId][0];
-		else
-			this._actorTpName = null;
+		var actor = $gameActors.actor(actorId);
+		var name = undefined;
+
+		if (actor.currentClass().meta.TPName)
+			name = actor.currentClass().meta.TPName;
+		if (actor.actor().meta.TPName)
+			name = actor.actor().meta.TPName;
+
+		if (name && name[0] === ' ') name = name.slice(1);
+		this._actorTpName = name;
 	};
 })();
